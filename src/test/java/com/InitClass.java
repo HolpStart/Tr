c5ad85e61c15961c70b7fadb54bb10f4dc02d161
jacType <a1234567>
2019-03-26 13:52:54 +0800

    da

---
+++ src/test/java/com/InitClass.java
@@ -0,0 +1,11 @@
+package com;
+
+import javafx.scene.chart.PieChart;
+
+import java.util.Date;
+
+public class InitClass {
+    public static void main(String[] args) {
+        System.out.println("============"+new Date().toLocaleString());
+    }
+}
